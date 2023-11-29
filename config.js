5903fb1150135c9596fa0e9bb62ab8fe9d7c61aa
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2023-11-29 18:06:45 +0100

    config.js en 1er puis script.js dans le head du HTML

---
+++ config.js
@@ -0,0 +1,3 @@
+// Liste des mots // Liste des mots et des phrases 
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ['Pas de panique !', "La vie, L'univers et le reste", "Merci pour le poisson"]
