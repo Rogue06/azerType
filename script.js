6300432b706d7371edffdbbd60f42324bdcfa062
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2023-11-29 17:21:51 +0100

    ajout fichier index.html & script.js avec un début de code sur script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+
+for(let i = 0; i < listeMots.length; i++) {
+    let inputUser = prompt("Entrez le mot " +listeMots[i])
+    if(inputUser === listeMots[i]) {
+        score++
+    }
+}
+
+console.log("Votre score est de : " + score + " sur " + listeMots.length);
+
+
+function retournerMessageScore(score, nombreQuestions) {
+    let message = 'Votre score est de ' + score + ' sur ' + nombreQuestions
+    return message
+}
+
+
+let nouveauMessage = retournerMessageScore(5, 10)
+console.log(nouveauMessage)
